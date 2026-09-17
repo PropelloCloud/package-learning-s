6df554a6b8a18a3ed66c8e8f4e97573a68cbb94d
sorayataraszka <sorayataraszka@propellocloud.com>
2026-09-17 11:39:33 +0100

    configuration - change the command file's name

--- src/Commands/PackageLearningSCommand.php
+++ src/Commands/PackageLearningSCommand.php
@@ -1,12 +1,12 @@
 <?php
 
-namespace VendorName\Skeleton\Commands;
+namespace Sorayataraszka\PackageLearningS\Commands;
 
 use Illuminate\Console\Command;
 
-class SkeletonCommand extends Command
+class PackageLearningSCommand extends Command
 {
-    public $signature = 'skeleton';
+    public $signature = 'package-learning-s';
 
     public $description = 'My command';
 
