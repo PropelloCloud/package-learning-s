c5ebe5262d72d274eced46a0a2590b41a3208098
sorayataraszka <sorayataraszka@propellocloud.com>
2026-09-21 11:31:50 +0100

    create the model

--- src/Models/AuditLogEntry.php
+++ src/Models/AuditLogEntry.php
@@ -2,7 +2,29 @@
 
 namespace Propello\PackageLearningS\Models;
 
-class AuditLogEntry
-{
+use Illuminate\Database\Eloquent\Model;
 
+class AuditLogEntry extends Model
+{
+    const UPDATED_AT = null;
+
+    protected $table = 'multi_audit_log_entries';
+
+    protected $fillable = [
+        'group_name',
+        'group_id',
+        'auditable_type',
+        'event',
+        'old_values',
+        'new_values',
+        'user_id',
+    ];
+
+    protected function casts(): array
+    {
+        return [
+            'old_values' => 'array',
+            'new_values' => 'array',
+        ];
+    }
 }
