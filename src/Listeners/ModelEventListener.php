13675a44cb5cb6703ee41782ffd1746e316ff255
sorayataraszka <sorayataraszka@propellocloud.com>
2026-09-21 13:46:42 +0100

    add event listeners

--- src/Listeners/ModelEventListener.php
+++ src/Listeners/ModelEventListener.php
@@ -2,7 +2,25 @@
 
 namespace Propello\PackageLearningS\Listeners;
 
+use Illuminate\Database\Eloquent\Model;
+use Propello\PackageLearningS\AuditManager;
+
 class ModelEventListener
 {
+    public function __construct(protected AuditManager $manager) {}
 
+    public function created(Model $model): void
+    {
+        $this->manager->record($model, 'created');
+    }
+
+    public function updated(Model $model): void
+    {
+        $this->manager->record($model, 'updated');
+    }
+
+    public function deleted(Model $model): void
+    {
+        $this->manager->record($model, 'deleted');
+    }
 }
